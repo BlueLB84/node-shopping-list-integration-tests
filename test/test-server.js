524a1c6f5e6bda67074cc3aa74c65a86821e750b
L Barbee <bluelb84@gmail.com>
2017-09-24 12:43:00 -0400

    adding Recipes test to master

--- test/test-server.js
+++ test/test-server.js
@@ -138,3 +138,86 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+  
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list recipes on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('should add a recipe on POST', function() {
+    const newRecipe = {name: 'banana pancakes', ingredients: ['banana', 'egg', 'vanilla']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+      });
+  });
+
+  // it('should raise error if an arg is missing', function() {
+  //   const badRecipe = {name: 'banana pancakes'};
+  //   return chai.request(app)
+  //     .post('/recipes')
+  //     .send(badRecipe)
+  //     .then(function(res) {
+  //       res.should.have.status(400);
+  //     });
+  // });
+
+
+  it('should update recipe on PUT', function() {
+    const updateData = {
+      name: 'chocolate chip banana pancakes',
+      ingredients: ['banana', 'egg', 'vanilla', 'chocolate chips']
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+  it('should delete recipe on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
